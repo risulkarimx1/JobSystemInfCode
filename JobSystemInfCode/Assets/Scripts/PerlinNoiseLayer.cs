8a485a032651bc90aaa9274d901130849e5cb5a8
risulkarimx1 <risulkarimx1@gmail.com>
2019-03-05 00:11:48 +0100

    [1] Added Perlin Noise in a Unity plane and running on CPU

---
+++ JobSystemInfCode/Assets/Scripts/PerlinNoiseLayer.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[Serializable]
+public class PerlinNoiseLayer
+{
+    public float Scale;
+    public float Speed;
+    public float Height;
+}
---
+++ JobSystemInfCode/Assets/Scripts/Simulation.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class Simulation : MonoBehaviour
+{
+    [SerializeField] private MeshFilter _meshFilter;
+
+    [SerializeField] private List<PerlinNoiseLayer> _perlinNoiseLayers;
+    // Start is called before the first frame update
+    private void Awake()
+    {
+        _meshFilter = GetComponent<MeshFilter>();
+    }
+
+    void Start()
+    {
+        _meshFilter.mesh.MarkDynamic();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var vertices = _meshFilter.mesh.vertices;
+
+        FlattenMesh(vertices);
+
+        foreach (var layer in _perlinNoiseLayers)
+        {
+            AddPerlinNoise(vertices, layer, Time.timeSinceLevelLoad);
+        }
+        _meshFilter.mesh.SetVertices(vertices.ToList());
+        _meshFilter.mesh.RecalculateNormals();
+    }
+    private void FlattenMesh(Vector3[] vertices)
+    {
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i].y = 0;
+        }
+    }
+
+    private void AddPerlinNoise(Vector3[] vertices, PerlinNoiseLayer layer, float time)
+    {
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            var x = vertices[i].x * layer.Scale + time * layer.Speed;
+            var z = vertices[i].z * layer.Scale + time * layer.Speed;
+
+            vertices[i].y += (Mathf.PerlinNoise(x, z) - 0.5f) * layer.Height;
+        }
+    }
+
+}
